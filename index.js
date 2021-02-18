85918b57accc458799f64cf481857ec4f80d6087
alireza944 <alirezahajizade944@gmail.com>
2021-02-18 18:47:31 +0330

    project2

---
+++ index.js
@@ -0,0 +1,16 @@
+let str = String.fromCharCode(9733)+" ";
+for(let i=1;i<=10;i++){
+    console.log(str.repeat(i))
+}
+
+
+
+let str1 = "hello world";
+console.log(str1.substring(6));
+console.log(str1.substring(0,5));
+
+
+
+let str2 = "HeLlo WorLd ";
+console.log(str2.toLocaleLowerCase());
+console.log(str2.toLocaleUpperCase());
